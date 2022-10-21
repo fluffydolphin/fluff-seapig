7f939fa838b1ec2a1f3719e58c0ad6a67bc67647
fluffydolphin <baxterhoey@gmail.com>
2022-10-22 02:47:16 +1300

    fix

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const path = require('path');
-module.exports = (req, res, next) => res.sendFile(path.join(__dirname, 'index.html'));
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('error: file not found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('something went wrong', error)
+    } else {
+        console.log('server listening on port: ' + port)
+    }
+})
